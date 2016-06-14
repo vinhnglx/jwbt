fcb2cd56c7f7394097e140e1b4e5f16f379d9b05
Vinh Nguyen <vinh.nglx@gmail.com>
2016-06-15 01:19:10 +0800

    Create API::TWToken module to create token base on scopes type

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class V1::SessionsController < V1::BaseController
+  before_filter :scopes, except: :create
   include ::V1::Session::Parameter
 
   def create
@@ -6,27 +7,13 @@ class V1::SessionsController < V1::BaseController
 
     return head :unauthorized unless user && user.authenticate(session_params[:password])
 
-    render json: { message: "Yo, You logged in!", token: token }, status: :ok
+    email = user.email
+
+    if email.include?("@example.com")
+      render json: { message: "Yo, You logged in!", token: API::JWToken.new('special').token }, status: :ok
+    else
+      render json: { message: "Yo, You logged in!", token: API::JWToken.new('general').token }, status: :ok
+    end
   end
-
-  private
-
-    # Create JWT base on payload and secret_key
-    #
-    # @returns String
-    def token
-      JWT.encode(payload, ENV['JWT_SECRET'], 'HS256')
-    end
-
-    # Create payload in JWT
-    #
-    # @returns Hash
-    def payload
-      {
-        exp: Time.now.to_i + 60 * 60,
-        iat: Time.now.to_i,
-        iss: ENV['JWT_ISSUER'],
-        scopes: ['list_products', 'show_products']
-      }
-    end
 end
+
